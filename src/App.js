bf4a2f37e7d200a2c70ddf2934db2bc7d68c85d7
lnellyee <nellykaikai@gmail.com>
2022-06-11 16:53:02 +0800

    fix broken build, updated landing page text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, my name is Nelly!
+          Hi, my name is actually kaikai
         </p>
         <a
           className="App-link"
